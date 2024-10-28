aab9c6d4255efed004a347adc6ba3360027a18b5
Mark Mcloughlin <mark25mky@gmail.com>
2024-10-28 18:09:29 +0000

    deployment commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: guricorn newssite.wsgi
---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://uatgnuff8lt:Zk4G8uvjOOGX@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/trump_shy_avert_205112"
+
+os.environ["SECRET_KEY"]="Reddit_secret_01"
+
+os.environ["CLOUDINARY_URL"] = "cloudinary://<your_api_key>:<your_api_secret>@djangoblog"
