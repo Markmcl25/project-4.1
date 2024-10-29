b21fac11c9a20f3b8ec3d89a2e73ecacf04fe90a
Mark Mcloughlin <mark25mky@gmail.com>
2024-10-29 22:01:18 +0000

    added list filter, readonly fields and ordering

--- reddit/admin.py
+++ reddit/admin.py
@@ -10,6 +10,16 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on')
     search_fields = ('title', 'content', 'slug') # Add search functionality
     ordering = ('-created_on',) # Orders by created_on in descending order
-    summernot_fields = ('content')
+    summernot_fields = ('content',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on', 'post')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments', 'disapprove_comments']
+    ordering = ('-created_on',)  # Orders by created_on in descending order
+    date_hierarchy = 'created_on'  # Add date hierarchy for filtering
+
 
 
