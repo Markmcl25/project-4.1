603b4ca9ed48029421b8401229f9d4a7e5a2c2ad
Mark Mcloughlin <mark25mky@gmail.com>
2024-10-30 13:58:57 +0000

    created views.py and templates

--- newssite/urls.py
+++ newssite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from reddit.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', home, name='home'),
 ]
