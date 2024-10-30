9e326c0916da0fcca49341654a8639142cb9ee51
Mark Mcloughlin <mark25mky@gmail.com>
2024-10-30 15:19:44 +0000

    added variables to env.py so posts can be created

--- env.py
+++ env.py
@@ -2,4 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://uatgnuff8lt:Zk4G8uvjOOGX@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/trump_shy_avert_205112"
 os.environ["SECRET_KEY"]="Reddit_secret_01"
-os.environ["CLOUDINARY_URL"] = "cloudinary://<your_api_key>:<your_api_secret>@reddit-project"
+os.environ["CLOUDINARY_URL"] = "cloudinary://137198229892871:W5_GMjA0LNTm9y6HGyqJtLD2mjk@reddit-project"
