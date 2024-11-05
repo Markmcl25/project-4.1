da943dfa73fdebe6466b88452f1fc6cbc9d28910
Mark Mcloughlin <mark25mky@gmail.com>
2024-11-05 14:22:45 +0000

    updated requirments and sellings files and installed django-allauth

--- newssite/urls.py
+++ newssite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', home, name='home'),
+    path('accounts/', include('allauth.urls')),
 ]
