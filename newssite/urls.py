79676bdaed435ef00198086c77d31e39867d5f5c
Mark Mcloughlin <mark25mky@gmail.com>
2024-11-07 14:10:01 +0000

    added new vairable to signup

--- newssite/urls.py
+++ newssite/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from reddit.views import home
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', home, name='home'),
     path('accounts/', include('allauth.urls')),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
