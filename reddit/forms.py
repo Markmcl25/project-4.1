4e18a469d283ca831e3d8c94652e929cc2547bc0
Mark Mcloughlin <mark25mky@gmail.com>
2024-11-12 15:50:08 +0000

    updated category form to be dynamic and fill with categories from the DB

--- reddit/forms.py
+++ reddit/forms.py
@@ -12,3 +12,5 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'category']  
+
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Select a Category")        
