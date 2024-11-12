797a618215a6684c815092f5416efb1692100b7f
Mark Mcloughlin <mark25mky@gmail.com>
2024-11-12 14:59:48 +0000

    updated forms page so categories are avialble for posting

--- reddit/forms.py
+++ reddit/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from .models import Post  
+from .models import Post, Category  
 
 class PostForm(forms.ModelForm):
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        empty_label="Select Category",
+        required=True,
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+
     class Meta:
         model = Post
-        fields = ['title', 'content', 'category']  
+        fields = ['title', 'content', 'category']  # Include 'category' in the fields list
