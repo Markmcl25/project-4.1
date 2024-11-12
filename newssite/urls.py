79d5cda97c5c4bfcdee75e2d462f3fe0c6d0d8cc
Mark Mcloughlin <mark25mky@gmail.com>
2024-11-12 14:07:56 +0000

    updated url page with create post

--- newssite/urls.py
+++ newssite/urls.py
@@ -4,8 +4,7 @@ from django.shortcuts import redirect
 from reddit.views import (
     PostList, PostDetail, custom_signup, signup_confirmation, create_post, LoggedOutView, category_view, edit_post
 )
-from django.contrib.auth.views import LogoutView, LoginView  
-
+from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,16 +15,13 @@ urlpatterns = [
     path('home/', lambda request: redirect('home')),  # Redirect /home to the homepage
 
     # Create post view
-    path('create_post/', create_post, name='create_post'),
+    path('create/', create_post, name='create_post'),  # URL for creating a new post
 
     # Category view
     path('category/<slug:category_slug>/', category_view, name='category'),  # Directly using category_view
 
     # Post detail view
     path('post/<int:pk>/', PostDetail.as_view(), name='post_detail'),
-
-    # Post Creation Page
-    path('post/edit/<int:pk>/', edit_post, name='create_post'),
 
     # Custom signup page
     path('accounts/signup/', custom_signup, name='account_signup'),
