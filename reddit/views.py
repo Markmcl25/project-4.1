32e94eb9e36db6a2d75d7e0da0877d8fc5fa0bf1
Mark Mcloughlin <mark25mky@gmail.com>
2024-11-26 18:27:40 +0000

    updated categories page to show seperate categories and users can also click on each post from within this menu

--- reddit/views.py
+++ reddit/views.py
@@ -129,142 +129,14 @@ def edit_post(request, pk):
     return render(request, 'edit_post.html', {'form': form, 'post': post})
 
 # Category view
-from django.http import JsonResponse
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView, TemplateView
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib import messages
-from django.contrib.auth import login
-from django.urls import reverse_lazy
-from .models import Post, Category, Comment
-from .forms import PostForm, CommentForm, SignupForm  # Import CommentForm
-
-# Class-based view for listing all posts (public view)
-class PostList(ListView):
-    model = Post
-    template_name = 'post_list.html'
-    context_object_name = 'posts'
-    paginate_by = 6
-
-    def get_queryset(self):
-        return Post.objects.filter(status=1).order_by('-created_on')
-
-def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()  # Pass categories to the template
-        return context        
-
-# Class-based view for listing posts by the logged-in user
-class UserPostListView(ListView):
-    model = Post
-    template_name = 'post_list.html'  # Template to render posts
-    context_object_name = 'posts'
-    paginate_by = 10
-
-    def get_queryset(self):
-        if self.request.user.is_authenticated:
-            return Post.objects.filter(author=self.request.user).order_by('-created_on')
-        return Post.objects.none()
-
-# Class-based view for creating a new post
-class CreatePostView(CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'create_post.html'
-    success_url = reverse_lazy('posts')
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user  # Set the logged-in user as the author
-        form.instance.status = 1  # Mark the post as published
-        form.save()
-        messages.success(self.request, 'Post created successfully!')
-        return redirect(self.success_url)
-
-# Class-based view for viewing a single post with comments
-class PostDetail(DetailView):
-    model = Post
-    template_name = 'post_detail.html'
-    context_object_name = 'post'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['comments'] = self.object.comments.filter(approved=True)  # Approved comments only
-        context['form'] = CommentForm()  # Provide the comment form
-        return context
-
-class CategoriesListView(ListView):
-    model = Category
-    template_name = 'category.html'
-    context_object_name = 'categories'
-
-# Handle comment submission for a post
-def add_comment(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.save()
-            messages.success(request, 'Your comment has been added and is awaiting approval.')
-        else:
-            messages.error(request, 'Please correct the errors in the form.')
-    return redirect('post_detail', pk=post.pk)
-
-# Class-based view for logged-out users
-class LoggedOutView(TemplateView):
-    template_name = 'logged_out.html'
-
-# Function-based view for the custom signup page
-def custom_signup(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            user = form.save(request)
-            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-            messages.success(request, "You have successfully signed up!")
-            return redirect('home')
-        else:
-            messages.error(request, "Please correct the errors below.")
-    else:
-        form = SignupForm()
-    return render(request, 'accounts/signup.html', {'form': form})
-
-# Custom login view
-def custom_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            messages.success(request, 'You have successfully logged in!')
-            return redirect('home')  # Replace 'home' with your desired redirect URL
-        else:
-            messages.error(request, 'Invalid username or password.')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'accounts/login.html', {'form': form})
-
-# Edit post view
-@login_required
-def edit_post(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            form.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'edit_post.html', {'form': form, 'post': post})
-
-# Category view
-def category_list(request, category_slug):
-    category = get_object_or_404(Category, slug=category_slug)
-    posts = Post.objects.filter(category=category, status=1)
+def category_list(request, category_slug=None):
     categories = Category.objects.all()
-    return render(request, 'category.html', {'category': category, 'posts': posts, 'category': categories})
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        posts = Post.objects.filter(category=category, status=1)
+        return render(request, 'category.html', {'category': category, 'posts': posts, 'categories': categories})
+    else:
+        return render(request, 'categories.html', {'categories': categories})
 
 # Signup confirmation page
 def signup_confirmation(request):
